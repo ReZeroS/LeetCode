b1e27558e80dfdac15baa4af676c64c1f4ef8544
ReZero <1726542850@qq.com>
2018-03-20 13:55:49 +0800

    Create answer.cpp
    Much more simple and beautiful.

---
+++ 2. Add two numbers/answer.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// To speed up the std::in and std::out
+static const auto _____ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode *head = new ListNode(0);
+        ListNode *tail = head;
+        
+        int carry = 0;
+        while(l1 || l2 || carry){
+            int n = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            tail -> next = new ListNode(n % 10);
+            carry = n / 10;
+            tail = tail -> next;
+            l1 = l1?l1 -> next:NULL;
+            l2 = l2?l2 -> next:NULL;
+        }
+        return head -> next;      
+        
+        
+    }
+};
