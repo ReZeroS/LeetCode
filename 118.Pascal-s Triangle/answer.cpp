e749cbde0a7e676b6467ae52be4ab5e781824899
ReZero <1726542850@qq.com>
2018-04-02 13:33:54 +0800

    Create answer.cpp

---
+++ 118.Pascal-s Triangle/answer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int> > ret;
+        if(numRows == 0)
+            return ret;
+            
+        vector<int> last(1,1);
+        ret.push_back(last);
+        for(int i = 2; i <= numRows; i ++)
+        {// i_th level
+            last.push_back(0);
+            vector<int> cur = last;
+            for(int j = 1; j < i; j ++)
+            {
+                cur[j] = last[j] + last[j-1];
+            }
+            ret.push_back(cur);
+            last = cur;
+        }
+        return ret;
+    }
+};
