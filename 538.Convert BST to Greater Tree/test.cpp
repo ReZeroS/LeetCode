a7a73938efc9801d012bf3914ed2821a1fd4dc01
ReZero <1726542850@qq.com>
2018-10-17 10:00:26 +0800

    Create test.cpp

---
+++ 538.Convert BST to Greater Tree/test.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int cur = 0;
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root){
+            return nullptr;
+        }
+        
+        convertBST(root->right);
+        root->val += cur;
+        cur = root->val;
+        convertBST(root->left);
+        
+        return root;
+        
+    }
+};
