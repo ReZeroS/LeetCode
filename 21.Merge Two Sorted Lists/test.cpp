c815174cd9fad569a9e4b86e7871fb97555ed2ba
ReZero <ljrezero@gmail.com>
2019-04-26 20:39:47 +0800

    Update test.cpp

--- 21.Merge Two Sorted Lists/test.cpp
+++ 21.Merge Two Sorted Lists/test.cpp
@@ -9,14 +9,12 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == nullptr || l2 == nullptr) {    
-            if(l1 == nullptr) return l2;
-            if(l2 == nullptr) return l1;
-            if(l1 == nullptr && l2 == nullptr) return nullptr;
-        }
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1 == nullptr && l2 == nullptr) return nullptr;
         ListNode* root = new ListNode(l1->val < l2->val ? l1->val : l2->val);
-        ListNode* ans = root;
         l1-> val < l2->val ? l1 = l1->next : l2 = l2->next;
+        ListNode* ans = root;
         while(l1 && l2){
             if(l1->val < l2->val) {
                 root->next = new ListNode(l1->val);
