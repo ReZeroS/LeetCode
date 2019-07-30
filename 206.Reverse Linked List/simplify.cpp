d0af1fb9f2ed12aa2f19cef8f97d6407fe2d82d9
ReZero <ljrezero@gmail.com>
2019-07-30 14:22:56 +0800

    Create simplify.cpp

---
+++ 206.Reverse Linked List/simplify.cpp
@@ -0,0 +1,10 @@
+ ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL, *cur=head, *tmp;
+        while(cur){
+            tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        return prev;
+   }
