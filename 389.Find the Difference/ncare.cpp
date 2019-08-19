170eca34dd872c7171315d7547435c333e04c88b
ReZero <ljrezero@gmail.com>
2019-08-19 09:05:56 +0800

    Create ncare.cpp

---
+++ 389.Find the Difference/ncare.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int a[26] = {0};
+        int b[26] = {0};
+        for(auto c : s) {
+            a[c - 'a'] += 1;
+        }
+        for(auto c : t) {
+            b[c - 'a'] += 1;
+        }
+        for(int i = 0; i < 26; ++i) {
+            if(a[i] < b[i]) {
+                return 'a' + i;
+            }
+        }
+        return 'a';
+    }
+};
