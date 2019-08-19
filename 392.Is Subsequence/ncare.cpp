31a06e349b93f51a6890cc85cc55f2022b397667
ReZero <ljrezero@gmail.com>
2019-08-19 10:25:58 +0800

    Create ncare.cpp

---
+++ 392.Is Subsequence/ncare.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        
+        while(true) {
+            if(i == s.size()) return true;
+            if(j == t.size()) return false;
+            if(s[i] == t[j]) {
+                i++, j++;
+            } else {
+                while(s[i] != t[j] && j < t.size()) ++j;
+            }
+        }
+        
+    }
+};
