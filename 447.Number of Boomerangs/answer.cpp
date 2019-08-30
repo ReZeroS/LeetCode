bd50782d5d84091619390908ea760d6b440a9ac8
ReZero <ljrezero@gmail.com>
2019-08-30 10:05:22 +0800

    Update answer.cpp

--- 447.Number of Boomerangs/answer.cpp
+++ 447.Number of Boomerangs/answer.cpp
@@ -2,12 +2,15 @@ a[0] = 0;           // init
 cout << (2*a[0]++); // 0
 cout << (2*a[0]++); // 2
 
-int numberOfBoomerangs(vector<pair<int, int>>& points) {
-    int booms = 0;
-    for (auto &p : points) {
-        unordered_map<double, int> ctr(points.size());
-        for (auto &q : points)
-            booms += 2 * ctr[hypot(p.first - q.first, p.second - q.second)]++;
-    }
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int booms = 0;
+        for (auto &p : points) {
+            unordered_map<double, int> ctr(points.size());
+            for (auto &q : points)
+                booms += 2 * ctr[hypot(p[0] - q[0], p[1] - q[1])]++;
+        }
     return booms;
-}
+    }
+};
