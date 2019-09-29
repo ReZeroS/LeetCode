cf1d0d249d606198f2e3fdf154297d5142e7671a
ReZero <ljrezero@gmail.com>
2019-09-29 17:36:19 +0800

    Create test.cpp

---
+++ 461.Hamming Distance/test.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int dist = 0, n = x ^ y;
+        while (n) {
+            ++dist;
+            n &= n - 1;
+        }
+        return dist;
+    }
+};
+
+
+// n&n-1 means turns off(set to 0) the right most 1 bit
