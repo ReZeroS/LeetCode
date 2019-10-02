a67fec6013376c5b6f4707720c7f6295bde93ef8
ReZero <ljrezero@gmail.com>
2019-10-02 19:58:38 +0800

    Update answer.cpp

--- 463.Island Perimeter/answer.cpp
+++ 463.Island Perimeter/answer.cpp
@@ -19,3 +19,7 @@ public:
     }
     
 };
+
+
+find how many 1 in the map. If without the consideration of surrounding cells, the total perimeter should be the total amount of 1 times 4.
+find how many cell walls that connect with both lands. We need to deduct twice of those lines from total perimeter
