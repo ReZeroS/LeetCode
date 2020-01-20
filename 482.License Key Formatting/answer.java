74ca861f64f68389eda6079248f46cb1a35e69c5
ReZero <ljrezero@gmail.com>
2020-01-20 16:37:14 +0800

    Update answer.java

--- 482.License Key Formatting/answer.java
+++ 482.License Key Formatting/answer.java
@@ -7,3 +7,10 @@ class Solution {
         return sb.reverse().toString().toUpperCase();
     }
 }
+
+
+// sb.length() % (k + 1) == k 
+// The for loop and the if condition together skip the '-' in the input string and whenever 
+// the sb StringBuilder is a multiple of k we add '-' to the string builder. sb.length() % (k + 1) == k
+// is a way to check if it's a multiple of k or not using modulo arithmetic.
+
